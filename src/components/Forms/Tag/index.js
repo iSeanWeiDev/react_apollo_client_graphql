b70e266bbde1d935b5df37d04aca7bbaa099155d
myo-work <myothihawork730@gmail.com>
2021-04-07 19:39:01 +0300

    created Tag form

--- src/components/Forms/Tag/index.js
+++ src/components/Forms/Tag/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Box, TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import testData from './data';
 import useStyles from './style';
 
 const TagForm = () => {
   const classes = useStyles();
+  const [loadedData, setLoadedData] = useState(testData.data);
 
-  return <Box className={classes.root}>Tag Form</Box>;
+  const handleInputChange = (e) => {
+    setLoadedData([
+      ...loadedData,
+      { label: e.target.value, value: e.target.value }
+    ]);
+  };
+
+  const handleTagChange = (e, value) => {
+    console.log(value);
+  };
+
+  return (
+    <Box className={classes.root}>
+      <Autocomplete
+        multiple
+        id="size-small-outlined-multi"
+        size="small"
+        options={loadedData}
+        getOptionLabel={(option) => option.label}
+        onChange={handleTagChange}
+        defaultValue={[loadedData[13]]}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label="tags"
+            onChange={handleInputChange}
+          />
+        )}
+      />
+    </Box>
+  );
 };
 
 export default TagForm;
